9959c612110a28caabf0c63abba3e89fc2ab3c3c
Filip Mucko <filip.mucko@gmail.com>
2019-03-23 09:42:19 +0100

    gitignore fix

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author fmucko
+ */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
